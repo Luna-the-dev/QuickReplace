0c66b9cb5c2b588e0cca6399816e7a0f50a5de42
Christian Norris <biguitar79@gmail.com>
2024-04-10 15:55:01 -0400

    initial commit

---
+++ Core/ObservableObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TextReplace.Core
+{
+    // used for updating the UI when binding. call OnPropertyChanged() in the setter for a property
+    // it seems similar to react states
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
