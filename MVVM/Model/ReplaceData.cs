73bf1cbc55ceac2d4ec5addd6314edc217b17050
Christian Norris <biguitar79@gmail.com>
2024-04-27 05:32:01 -0400

    added option so search for whole words

--- MVVM/Model/ReplaceData.cs
+++ MVVM/Model/ReplaceData.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualBasic.FileIO;
 using TextReplace.Core.AhoCorasick;
 using System.Diagnostics;
 using System.IO;
+using System.Windows.Shapes;
 
 namespace TextReplace.MVVM.Model
 {
@@ -26,6 +27,7 @@ namespace TextReplace.MVVM.Model
             }
         }
 
+        // flag for whether the search should be case sensitive
         private bool _caseSensitive = false;
         public bool CaseSensitive
         {
@@ -33,17 +35,20 @@ namespace TextReplace.MVVM.Model
             set { _caseSensitive = value; }
         }
 
+        // delimiters which decides what seperates whole words
+        private const string DELIMITERS = " \t/\\()\"'-:,.;<>~!@#$%^&*|+=[]{}?│";
+
         public ReplaceData(bool caseSensitive)
         {
             CaseSensitive = caseSensitive;
         }
 
-        public bool SaveReplacePhrases()
+        public bool ParseReplacePhrases()
         {
             try
             {
-                // if file is a XSV
-                ReplacePhrases = ParseXSV(ReplaceFile.FileName);
+                // if file is formatted as a delimiter seperated value
+                ReplacePhrases = ParseDSV(ReplaceFile.FileName);
             }
             catch
             {
@@ -54,14 +59,14 @@ namespace TextReplace.MVVM.Model
         }
 
         /// <summary>
-        /// Parses "seperated value" files such as .csv or .tsv. Defaults to .csv files.
+        /// Parses "delimiter seperated value" files such as .csv or .tsv. Defaults to .csv files.
         /// </summary>
         /// <param name="delimiter"></param>
         /// <returns>
         /// A dictionary of pairs of the values from the file. If one of the lines in the file has an
         /// incorrect number of values or if the operation fails for another reason, return an empty list.
         /// </returns>
-        public Dictionary<string, string> ParseXSV(string fileName, string delimiter = ",")
+        public Dictionary<string, string> ParseDSV(string fileName, string delimiter = ",")
         {
             var phrases = new Dictionary<string, string>();
 
@@ -82,12 +87,15 @@ namespace TextReplace.MVVM.Model
                         line = parser.ReadFields();
                         if (line == null || line.Length != 2)
                         {
+                            Debug.WriteLine("DSV replace file could not be parsed" +
+                                "due to having an invalid number of values in a row");
                             return new Dictionary<string, string>();
                         }
                         phrases[line[0]] = line[1];
                     }
-                    catch (MalformedLineException ex)
+                    catch (MalformedLineException e)
                     {
+                        Debug.WriteLine($"DSV replace file could not be parsed due to {e}");
                         return new Dictionary<string, string>();
                     }
                 }
@@ -106,7 +114,7 @@ namespace TextReplace.MVVM.Model
         /// <param name="srcFiles"></param>
         /// <param name="destFiles"></param>
         /// <returns>False if writing to one of the files failed.</returns>
-        public bool PerformReplacements(List<string> srcFiles, List<string> destFiles)
+        public bool PerformReplacements(List<string> srcFiles, List<string> destFiles, bool isWholeWord)
         {
             // construct the automaton and fill it with the phrases to search for
             // also create a list of the replacement phrases to go alongside the 
@@ -121,7 +129,7 @@ namespace TextReplace.MVVM.Model
             bool didEverythingSucceed = true;
             for (int i = 0; i < srcFiles.Count; i++)
             {
-                bool res = WriteReplacementsToFile(srcFiles[i], destFiles[i], matcher);
+                bool res = WriteReplacementsToFile(srcFiles[i], destFiles[i], matcher, isWholeWord);
                 if (res == false)
                 {
                     Debug.WriteLine("Something went wrong in PerformReplacements()");
@@ -133,36 +141,29 @@ namespace TextReplace.MVVM.Model
         }
 
         /// <summary>
-        /// Utility function used by PerformReplacements which will use the
-        /// Aho-Corasick string searcher to replace any matches found in the
-        /// source file to the destination file.
+        /// A wrapper function for MatchAndWrite() and MatchAndWriteWholeWord() in order
+        /// to cut down on the number of if/else checks inside of loops.
         /// </summary>
         /// <param name="src"></param>
         /// <param name="dest"></param>
         /// <param name="matcher"></param>
+        /// <param name="isWholeWord"></param>
         /// <returns>False is some exception was thrown.</returns>
-        private bool WriteReplacementsToFile(string src, string dest, AhoCorasickStringSearcher matcher)
+        private bool WriteReplacementsToFile(string src, string dest, AhoCorasickStringSearcher matcher, bool isWholeWord)
         {
             try
             {
                 using (var sw = new StreamWriter(dest))
                 {
-                    foreach (string line in File.ReadLines(src))
+                    // making these two seperate functions in order to cut down on if/else checks
+                    // inside the foreach loops (which could potentially loop millions of times each)
+                    if (isWholeWord)
                     {
-                        // search the current line for any text that should be replaced
-                        var matches = matcher.Search(line);
-
-                        // save an offset to remember how much the position of each replacement
-                        // should be shifted if a replacement was already made on the same line
-                        int offset = 0;
-                        string updatedLine = line;
-                        foreach (var m in matches)
-                        {
-                            updatedLine = updatedLine.Remove(m.Position + offset, m.Text.Length)
-                                                     .Insert(m.Position + offset, ReplacePhrases[m.Text]);
-                            offset += ReplacePhrases[m.Text].Length - m.Text.Length;
-                        }
-                        sw.WriteLine(updatedLine);
+                        MatchAndWriteWholeWord(src, sw, matcher);
+                    }
+                    else
+                    {
+                        MatchAndWrite(src, sw, matcher);
                     }
                 }
             }
@@ -174,6 +175,104 @@ namespace TextReplace.MVVM.Model
 
             return true;
         }
+
+        /// <summary>
+        /// Uses the Aho-Corasick algorithm to search a file for any substring matches
+        /// in a source file. This function is wrapped by WriteReplacementsToFile() and
+        /// called from PerformReplacements().
+        /// </summary>
+        /// <param name="src"></param>
+        /// <param name="sw"></param>
+        /// <param name="matcher"></param>
+        private void MatchAndWrite(string src, StreamWriter sw, AhoCorasickStringSearcher matcher)
+        {
+            foreach (string line in File.ReadLines(src))
+            {
+                // search the current line for any text that should be replaced
+                var matches = matcher.Search(line);
+
+                // save an offset to remember how much the position of each replacement
+                // should be shifted if a replacement was already made on the same line
+                int offset = 0;
+                string updatedLine = line;
+                foreach (var m in matches)
+                {
+                    updatedLine = updatedLine.Remove(m.Position + offset, m.Text.Length)
+                                             .Insert(m.Position + offset, ReplacePhrases[m.Text]);
+                    offset += ReplacePhrases[m.Text].Length - m.Text.Length;
+                }
+                sw.WriteLine(updatedLine);
+            }
+        }
+
+        /// <summary>
+        /// Uses the Aho-Corasick algorithm to search a file for any whole-word matches
+        /// in a source file. This function is wrapped by WriteReplacementsToFile() and
+        /// called from PerformReplacements().
+        /// </summary>
+        /// <param name="src"></param>
+        /// <param name="sw"></param>
+        /// <param name="matcher"></param>
+        private void MatchAndWriteWholeWord(string src, StreamWriter sw, AhoCorasickStringSearcher matcher)
+        {
+            foreach (string line in File.ReadLines(src))
+            {
+                // search the current line for any text that should be replaced
+                var matches = matcher.Search(line);
+
+                // save an offset to remember how much the position of each replacement
+                // should be shifted if a replacement was already made on the same line
+                int offset = 0;
+                string updatedLine = line;
+                foreach (var m in matches)
+                {
+                    if (IsMatchWholeWord(line, m.Text, m.Position) == false)
+                    {
+                        continue;
+                    }
+                    updatedLine = updatedLine.Remove(m.Position + offset, m.Text.Length)
+                                             .Insert(m.Position + offset, ReplacePhrases[m.Text]);
+                    offset += ReplacePhrases[m.Text].Length - m.Text.Length;
+                }
+                sw.WriteLine(updatedLine);
+            }
+        }
+
+        /// <summary>
+        /// Checks to see if a match found by the AhoCorasickStringSearcher
+        /// Search() method is a whole word.
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="text"></param>
+        /// <param name="pos"></param>
+        /// <returns>False if the character before and after the match exists and isnt a delimiter.</returns>
+        private bool IsMatchWholeWord(string line, string text, int pos)
+        {
+            /*
+             * yes, i know this is ugly. this can be boiled down to the following.
+             * match is not a whole word if:
+             *   there is a char before the match AND it is not in the list of delimiters
+             *   OR
+             *   there is a char after the match AND it is not found in the list of delimiters
+             */
+            int indexBefore = pos - 1;
+            int indexAfter = pos + text.Length;
+            if ( ( indexBefore >= 0
+                   &&
+                   DELIMITERS.Contains(line[indexBefore]) == false
+                 )
+                 ||
+                 (indexAfter < line.Length
+                 &&
+                   DELIMITERS.Contains(line[indexAfter]) == false
+                 )
+               )
+            {
+                return false;
+            }
+            return true;
+        }
+
 
     }
 }
--- MVVM/ViewModel/ReplaceViewModel.cs
+++ MVVM/ViewModel/ReplaceViewModel.cs
@@ -13,15 +13,25 @@ namespace TextReplace.MVVM.ViewModel
             private set { _caseSensitive = value; }
         }
 
+        private bool _wholeWord;
+
+        public bool WholeWord
+        {
+            get { return _wholeWord; }
+            private set { _wholeWord = value; }
+        }
+
+
         // commands
         public RelayCommand Replace { get; set; }
         public RelayCommand ToggleCaseSensitive { get; set; }
+        public RelayCommand ToggleWholeWord { get; set; }
 
         public ReplaceViewModel()
         {
             Replace = new RelayCommand(o =>
             {
-                if (ReplaceData() == false)
+                if (DoReplace() == false)
                 {
                     Debug.WriteLine("Something went wrong in the replace command.");
                 }
@@ -32,19 +42,25 @@ namespace TextReplace.MVVM.ViewModel
                 Debug.WriteLine($"case sensitive = {o}");
                 CaseSensitive = Convert.ToBoolean(o);
             });
+
+            ToggleWholeWord = new RelayCommand(o =>
+            {
+                Debug.WriteLine($"whole word = {o}");
+                WholeWord = Convert.ToBoolean(o);
+            });
         }
 
         /// <summary>
-        /// Utility function used by the Replace command.
+        /// Utility function used by the Replace command. This performs the actual replace function.
         /// </summary>
         /// <returns>Returns false if something went wrong.</returns>
-        private bool ReplaceData()
+        private bool DoReplace()
         {
             ReplaceData replaceData = new ReplaceData(CaseSensitive);
             string suffix = "replacify"; // TODO let the user change this with GUI later
 
-            // open a file dialogue for the user and update the source files
-            bool result = replaceData.SaveReplacePhrases();
+            // parse the replace phrases and save them in the object
+            bool result = replaceData.ParseReplacePhrases();
 
             if (result == false)
             {
@@ -56,7 +72,7 @@ namespace TextReplace.MVVM.ViewModel
             List<string> destFileNames = SourceFiles.GenerateDestFileNames(suffix);
 
             // perform the text replacements
-            result = replaceData.PerformReplacements(SourceFiles.FileNames, destFileNames);
+            result = replaceData.PerformReplacements(SourceFiles.FileNames, destFileNames, WholeWord);
 
             if (result == false)
             {
