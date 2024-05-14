e9e951c2bfd424d758b3dea9217d0ab4e7b398eb
Christian Norris <biguitar79@gmail.com>
2024-05-14 18:19:58 -0400

    fixed scroll wheel not working in replace view

--- MVVM/View/ReplaceView.xaml.cs
+++ MVVM/View/ReplaceView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.Diagnostics;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
 using TextReplace.MVVM.ViewModel;
@@ -48,6 +49,22 @@ namespace TextReplace.MVVM.View
             {
                 viewModel.SetDelimiter(dialog.InputText);
             }
+        }
+
+        /// <summary>
+        /// This function exists to make scroll wheel work for a scroll viewer with a list box
+        /// inside of it. It normally doesnt work out of the box because a list box's template
+        /// already contains a scroll viewer, but that has some of its own problems (namely the
+        /// inability to drag the scroll bar when an item is selected). This also allows me to
+        /// control the styling of the scroll viewer independently.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ListBoxScrollViewer_PreviewMouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            ScrollViewer scv = (ScrollViewer)sender;
+            scv.ScrollToVerticalOffset(scv.VerticalOffset - (e.Delta / 6));
+            e.Handled = true;
         }
     }
 }
