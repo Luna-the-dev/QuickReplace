4893ad11150d294adb882730d90cf750d34c59ea
Christian Norris <biguitar79@gmail.com>
2024-07-19 04:10:14 -0400

    added comment explaining why test might fail if file is not made unreadable

--- TextReplace.Tests/ViewModels/PopupWindows/Source/UploadSourceFilesInputTests.cs
+++ TextReplace.Tests/ViewModels/PopupWindows/Source/UploadSourceFilesInputTests.cs
@@ -85,6 +85,8 @@ namespace TextReplace.Tests.ViewModels.PopupWindows.Source
         [Theory]
         [InlineData("invalid-file-type-source.bin")]
         [InlineData("unreadable-source.txt")]
+        // NOTE: if the "unreadable-source.txt" test is failing, please make sure that the file is
+        // *not* readable. the file is located in TextReplace/TextReplace.Tests/MockFiles/MockSources.
         public void ValidateFiles_InvalidFile_SetFieldsForInvalidFiles(string filename)
         {
             // Arrange
